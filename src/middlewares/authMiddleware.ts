5f08dd55789d93e9617cc319651d34395239714a
vinspam <brandonconnor0314@gmail.com>
2024-03-21 02:15:36 +0530

    Fixed /me Route

--- src/middlewares/authMiddleware.ts
+++ src/middlewares/authMiddleware.ts
@@ -8,15 +8,24 @@ export const authMiddleware = async (req:Request & { user?: User }, res:Response
     const token = req.headers.authorization;
     if(!token){
         return res.status(401).json({message:"Invalid Authorization"});
+        
     }
 
-    const payload = jwt.verify(token, JWT_SECRET) as any;
-    const user = await prismaClient.user.findFirst({where: {id:payload.userId}})
+    try {
+        const payload = jwt.verify(token, JWT_SECRET) as any;
+        const user = await prismaClient.user.findFirst({where: {id:payload.userId}})
 
     if(!user){
         return res.status(401).json({message:"Invalid Authorization"});
         
     }
+    req.user = user
+    res.json({user})
+    next();
+    } catch (error) {
+        res.json({message:"Invalid Authorization"})
+        console.log("Invalid Authorizationror")
+    }
 
-    req.user = user;
+    
 }
